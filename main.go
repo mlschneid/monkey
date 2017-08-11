fb32aeb94edf58656c64fcad2f601e889ac57094
Mike Schneider <schneider707@gmail.com>
2017-08-11 10:54:19 -0700

    First test passing.

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	input := `let five = 5;	
+let ten = 10;
+let add = fn (x,y){
+	x + y;
+};
+
+let result = add(five, ten);
+`
+
+	fmt.Printf(input)
+
+}
---
+++ token/token.go
@@ -0,0 +1,33 @@
+package token
+
+type TokenType string
+
+// Token type
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + Literals
+	IDENT  = "IDENT"
+	INT    = "INT"
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Operators
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
