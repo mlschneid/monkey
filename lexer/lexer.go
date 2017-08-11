03b52ea050722e982b15b9118fcaddac61235f40
Mike Schneider <schneider707@gmail.com>
2017-08-11 15:07:26 -0700

    init.

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,6 +1,8 @@
 package lexer
 
 import (
+	"fmt"
+
 	"github.com/mlschneid/monkey/token"
 )
 
@@ -51,10 +53,33 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+
+			return tok
+		}
+
+		tok = newToken(token.ILLEGAL, l.ch)
+
 	}
 
 	l.readChar()
 	return tok
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+
+	fmt.Printf("Reading identified as: %v\n", l.input[position:l.position])
+	return l.input[position:l.position]
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
 }
 
 func newToken(tokenType token.TokenType, ch byte) token.Token {
